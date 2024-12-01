593b95b8d44939b9cf3c7d681a1a77d33d2adaa0
Michael <124744858+speyec@users.noreply.github.com>
2024-12-01 18:04:48 -0500

    Add files via upload

---
+++ basketball-data.sql
@@ -0,0 +1,123 @@
+--LA LAKERS TABLE
+INSERT INTO LaLakers VALUES (
+    '516 Million', 'Crypto.com Arena', 'Lebron James', 'Redick'
+);
+ 
+--POSITION TABLE
+INSERT INTO position VALUES (
+    'G'
+);
+ 
+INSERT INTO position VALUES (
+    'F'
+);
+ 
+INSERT INTO position VALUES (
+    'C'
+);
+ 
+--PLAYERS TABLE
+INSERT INTO player VALUES (
+    'Jalen', 'Hood-Schifino', 'G', 77, 215,'340372957', '19-JUN-2003','N'
+);
+ 
+INSERT INTO player VALUES (
+    'Lebron', 'James', 'F', 81, 250,'258271957', '30-DEC-1984','Y'
+);
+ 
+INSERT INTO player VALUES (
+    'Dangelo', 'Russell', 'G', 75, 193,'134862487', '23-FEB-1996', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Jarred', 'Vanderbilt', 'F', 80, 214,'174387549', '03-APR-1999', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Anthony', 'Davis', 'F', 82, 253,'073845937', '11-MAR-1993', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Dalton', 'Knecht', 'F', 78, 215,'391658937', '19-APR-2001', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Cam', 'Reddish', 'G', 79, 217,'621789463', '01-SEP-1999', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Gabe', 'Vincent', 'G', 74, 200,'847238085', '14-JUN-1996', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Bronny', 'James', 'G', 74, 210,'391265678', '06-OCT-2004', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Christian', 'Koloko', 'C', 84, 230,'734921436', '20-JUN-2000', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Jaxson', 'Hayes', 'C', 84, 220,'376190386', '23-MAY-2000', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Max', 'Christie', 'G', 77, 190,'456234890', '10-FEB-2003', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Austin', 'Reaves', 'G', 77, 197,'284567218', '29-MAY-1998', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Maxwell', 'Lewis', 'F', 79, 195,'083649264', '27-JUL-2002', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Rui', 'Hachimura', 'F', 80, 230,'153783907', '08-FEB-1998', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Christian', 'Wood', 'F', 80, 214,'604655823', '27-SEP-1995', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Quincy', 'Olivari', 'G', 75, 200,'826483992', '27-MAY-2001', 'N'
+);
+ 
+INSERT INTO player VALUES (
+    'Armel', 'Traore', 'F', 80, 215,'456237347', '23-JAN-2003', 'N'
+);
+ 
+--STAFF TABLE
+INSERT INTO staff VALUES (
+    'JJ', 'Redick', 'Head Coach','123456789', '24-JUN-1984', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Greg', 'St. Jean', 'Assistant Coach','123456789', '12-MAY-1990', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Scott', 'Brooks', 'Assistant Coach','987654321', '31-JUL-1965', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Nate', 'McMillan', 'Assistant Coach','123457689', '03-AUG-1964', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Lindsey', 'Harding', 'Assistant Coach','213456789', '12-JUN-1984', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Bob', 'Beyer', 'Assistant Coach','456289478', '10-DEC-1961', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Beau', 'Levesque', 'Assistant Coach','628749284', '26-MAY-1991', 'Redick'
+);
+ 
+INSERT INTO staff VALUES (
+    'Vanessa', 'Brooks', 'Trainer','529471286', '24-JUL-1987', 'Redick'
+);
